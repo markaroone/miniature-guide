57cc00ff9ea37c4956865a947fdd22cc9bf29bb7
Mark Christian Fajardo <cfajardo25@gmail.com>
2023-11-28 19:25:40 +0800

    Update app version to 0.0.15.

--- src/theme/horizon-theme.ts
+++ src/theme/horizon-theme.ts
@@ -1,5 +1,7 @@
 import { createTheme } from '@mui/material';
 
+import { getStatePropsValue } from '@/utils';
+
 import * as Themed from '../components';
 
 import { palette } from './palette';
@@ -55,6 +57,49 @@ export const horizonTheme = createTheme({
     MuiTextField: {
       defaultProps: {
         variant: 'outlined',
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme, ownerState }) => ({
+          '&.MuiInputBase-multiline': {
+            height: 'unset',
+          },
+          '& fieldset': {
+            borderColor: theme.palette.horizonGray3.main,
+            borderWidth: '1.5px',
+          },
+          '&&:hover fieldset': {
+            borderColor: getStatePropsValue(ownerState, {
+              disabled: 'text.disabled',
+              error: theme.palette.error.main,
+              default: theme.palette.horizonBlue.main,
+            }),
+          },
+          '&&.Mui-focused fieldset': {
+            borderColor: theme.palette.horizonBlue.main,
+            borderWidth: '1.5px',
+          },
+          '&.Mui-focused': {
+            '&.Mui-disabled fieldset': {
+              borderColor: 'text.disabled',
+            },
+            '&.Mui-error fieldset': {
+              borderColor: theme.palette.error.main,
+              borderWidth: '1.5px',
+            },
+          },
+        }),
+        input: ({ ownerState, theme }) => ({
+          '&::placeholder': {
+            color: theme.palette.horizonGray1.main,
+          },
+          color: getStatePropsValue(ownerState, {
+            disabled: 'text.disabled',
+            error: theme.palette.error.main,
+            default: theme.palette.text.primary,
+          }),
+        }),
       },
     },
     // MuiOutlinedInput: Themed.MuiOutlinedInput,
